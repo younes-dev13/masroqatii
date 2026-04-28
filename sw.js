51ab474141e521751e854c92dbe257e2c68160a5
younes-dev13 <youneshaouari2007@gmail.com>
2026-04-29 00:34:00 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='masruqati-pro-v1';const FILES=['./','./index.html','./style.css','./script.js','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
